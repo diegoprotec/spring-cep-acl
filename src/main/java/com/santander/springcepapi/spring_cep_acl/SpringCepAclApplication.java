d6c307427650c20a9e270b463057f39fa2f922a5
diego <diegosfprotec@gmail.com>
2025-04-28 22:56:13 -0300

    start

---
+++ src/main/java/com/santander/springcepapi/spring_cep_acl/SpringCepAclApplication.java
@@ -0,0 +1,13 @@
+package com.santander.springcepapi.spring_cep_acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCepAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCepAclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santander/springcepapi/spring_cep_acl/SpringCepAclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.springcepapi.spring_cep_acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCepAclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
